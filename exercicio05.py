0d9c155964b4ed8c08111b032dd1e5fe1e427a13
Marcondys Santos Bezerra Silva <marcondysbezerra@gmail.com>
2023-11-28 14:22:23 -0300

    Sexto commit

---
+++ exercicio05.py
@@ -0,0 +1,6 @@
+# Faça um Programa que converta metros para centímetros.
+
+metro = float(input("Digite o tamanho em metro: "))
+
+centimetro = metro * 100
+print(f"{metro} metro é igual a {centimetro} centímetro.")
