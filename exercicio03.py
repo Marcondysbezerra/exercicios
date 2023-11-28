c8c11c588a3be551d0249c24dcc44bef0d6395ce
Marcondys Santos <48830573+Marcondysbezerra@users.noreply.github.com>
2023-11-28 16:37:11 -0300

    Delete exercicio03.py

--- exercicio03.py
+++
@@ -1,7 +0,0 @@
-# Faça um Programa que peça dois números e imprima a soma.
-
-numero1 = int(input("Primeiro número: "))
-numero2 = int(input("Segundo número: "))
-
-soma = numero1 + numero2
-print(soma)
