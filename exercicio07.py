2e578a55ae7b2ec4a1ef0a1ffec0df2e600c40fb
Marcondys Santos <48830573+Marcondysbezerra@users.noreply.github.com>
2023-11-28 16:37:53 -0300

    Delete exercicio07.py

--- exercicio07.py
+++
@@ -1,8 +0,0 @@
-# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-
-lado1 = float(input("Digite o valor do lado 1: "))
-lado2 = float(input("Digite o valor do lado 2: "))
-
-area_quadrado = lado1 * lado2
-
-print(f"A áera do quadrado é: {area_quadrado}m²")
